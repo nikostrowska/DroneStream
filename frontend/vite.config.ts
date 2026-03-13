f7173363b425458521c98e0b56f01884dc0408a0
Krystian Czajkowski <36819859+krystianczajkowski@users.noreply.github.com>
2026-03-13 19:53:24 +0100

    Update dockerfile and vite.config, setup environmental variable loading

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,24 +1,28 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
+
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [["babel-plugin-react-compiler"]],
-      },
-    }),
-  ],
-  server: {
-    port: 4000,
-    strictPort: true,
-    host: true,
-    proxy: {
-      "/api": {
-        target: "http://backend:4001",
-        secure: false,
+export default defineConfig( ({mode}) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    plugins: [
+      react({
+        babel: {
+          plugins: [["babel-plugin-react-compiler"]],
+        },
+      }),
+    ],
+    server: {
+      port: 4000,
+      strictPort: true,
+      host: true,
+      proxy: {
+        "/api": {
+          target: `http://${env.BACKEND_HOSTNAME}:4001`,
+          secure: false,
+        },
       },
     },
-  },
+  }
 });
