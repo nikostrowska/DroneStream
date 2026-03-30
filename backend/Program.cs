0db00e6a8ee56c13df6a7b63b9021b547f97dfeb
Nikola <nik.ostrowska@gmail.com>
2026-03-30 19:56:27 +0200

    Add Swagger for API doc, disable HTTPS redirect (not needed for now), set env to Development.

--- backend/Program.cs
+++ backend/Program.cs
@@ -5,15 +5,20 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddOpenApi();
 builder.Services.AddControllers();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
 var summaries = new[]
 {
