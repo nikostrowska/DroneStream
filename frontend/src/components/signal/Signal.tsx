ecea469710276a196ca733142502883c609a8953
Krystian Czajkowski <krystianczajkowski01@gmail.com>
2026-04-27 20:00:25 +0200

    add correct types

--- frontend/src/components/signal/Signal.tsx
+++ frontend/src/components/signal/Signal.tsx
@@ -1,41 +1,35 @@
 import * as signalR from "@microsoft/signalr";
 import { useEffect, useState } from 'react';
+import type { DroneTelemetry } from "../map/MapContext";
 
 const messageComponent = () => {
-    const [connection, setConnection] = useState(null);
-    const [message, setMessage] = useState(null);
+    const [connection, setConnection] = useState<signalR.HubConnection | null>(null);
+    const [telemetry, setTelemetry] = useState<DroneTelemetry | null>(null);
     useEffect(() => {
-        const newConnection = new signalR.HubConnectionBuilder()
+        const newConnection: signalR.HubConnection = new signalR.HubConnectionBuilder()
             .withUrl("http://localhost:4001/droneTelemetryHub").build();
         setConnection(newConnection);
     }, []);
 
     useEffect(() => {
-        if (!connection) return
-        const handler = (dto) => {
+        if (!connection) return;
+        const handler = ({ dto }: { dto: DroneTelemetry }) => {
             console.log("Got a message", dto);
-            setMessage((message) => (dto));
+            setTelemetry(dto);
         };
         connection.start()
             .then(() => {
-            connection.invoke("SubscribeTopic", "test");
+                connection.invoke("SubscribeTopic", "test");
             }).catch(e => console.log("Connection failed: ", e));
 
-            connection.on("ReceiveTelemetry", handler);
+        connection.on("ReceiveTelemetry", handler);
 
     }, [connection]);
 
-    // useEffect(() => {
-    //   if (!message) {
-    //         return;
-    //   }
-    //   console.log("Messages updated:", message.data);
-    // }, [message]);
-    
     return (
         <div>
             <h2>
-                {JSON.stringify(message?.data?.longitude, message?.data.?latitude)}
+                {JSON.stringify(telemetry?.data.longitude)}, {JSON.stringify(telemetry?.data.latitude)}
             </h2>
         </div>
     );
