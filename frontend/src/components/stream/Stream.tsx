09a65840a525f8d88636da69a9307c4bc7ceb200
Krystian Czajkowski <36819859+krystianczajkowski@users.noreply.github.com>
2026-05-04 08:20:08 +0200

    Update iframe src

--- frontend/src/components/stream/Stream.tsx
+++ frontend/src/components/stream/Stream.tsx
@@ -2,7 +2,7 @@ export default function Stream({ droneName }: { droneName: string | null }) {
   return (
     <div className="max-h-[800px] relative w-full max-w-[1454px] aspect-video bg-black rounded-[32px] shadow-2xl border-2 border-white overflow-hidden mx-auto mt-5">
       <iframe
-        src={`http://localhost:8889/${droneName}`}
+        src={`http://${window.location.hostname}:8889/${droneName}`}
         className="w-full h-full rounded-[16px]"
         allow="autoplay"
         title="live stream"
