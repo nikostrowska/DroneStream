a4bab29382ac32b2329d031e708130489d3831f7
Krystian Czajkowski <36819859+krystianczajkowski@users.noreply.github.com>
2026-05-19 12:51:04 +0200

    Update DroneTelemetryHub, send cached data on subscribe

--- backend/Hubs/DroneTelemetryHub.cs
+++ backend/Hubs/DroneTelemetryHub.cs
@@ -1,13 +1,19 @@
-
 using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Caching.Memory;
 
+using backend.DTOs;
 namespace backend.Hubs
 {
-    public class DroneTelemetryHub : Hub
+    public class DroneTelemetryHub(IMemoryCache cache) : Hub
     {
         public async Task SubscribeTopic(string droneSn)
         {
             await Groups.AddToGroupAsync(Context.ConnectionId, droneSn);
+            if (cache.TryGetValue(droneSn, out DroneTelemetryDTO? cachedData))
+            {
+                await Clients.Group(droneSn).SendAsync("ReceiveTelemetry", cachedData);
+
+            }
         }
         public async Task UnsubscribeTopic(string droneSn)
         {
